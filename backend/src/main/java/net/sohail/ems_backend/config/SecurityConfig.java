aad80c24cc58b0c8c199933e70d07455892fa945
Sohail Khan Pattan <69030926+sohail9290@users.noreply.github.com>
2026-01-12 22:01:22 -0600

    Add allowed origin for production environment

--- backend/src/main/java/net/sohail/ems_backend/config/SecurityConfig.java
+++ backend/src/main/java/net/sohail/ems_backend/config/SecurityConfig.java
@@ -31,8 +31,9 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://localhost:5173"
+            "http://localhost:3000",
+            "http://localhost:5173",
+            "https://staff-sphere-v2.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
