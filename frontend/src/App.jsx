ef700e3e9d12fc1af10aded821c9fcccb422a91a
Sohail Khan Pattan <69030926+sohail9290@users.noreply.github.com>
2026-01-12 22:21:42 -0600

    Refactor routing structure in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,48 @@
 import './App.css'
-import EmployeeComponent from './components/EmployeeComponent'
-import FooterComponent from './components/FooterComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import LoginComponent from './components/LoginComponent'
+
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import EmployeeComponent from './components/EmployeeComponent'
 import ListDepartmentComponent from './components/ListDepartmentComponent'
 import DepartmentComponent from './components/DepartmentComponent'
-import LoginComponent from './components/LoginComponent';
+
 import RequireAuth from './auth/RequireAuth'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
+
         {/* PUBLIC */}
         <Route path="/login" element={<LoginComponent />} />
 
-        {/* PROTECTED */}
-        <Route
-          path="/*"
-          element={
-            <RequireAuth>
+        {/* PROTECTED LAYOUT */}
+        <Route element={<RequireAuth />}>
+          <Route
+            element={
               <>
                 <HeaderComponent />
-
-                <Routes>
-                  <Route path="/" element={<ListEmployeeComponent />} />
-                  <Route path="/employees" element={<ListEmployeeComponent />} />
-                  <Route path="/add-employee" element={<EmployeeComponent />} />
-                  <Route path="/edit-employee/:id" element={<EmployeeComponent />} />
-                  <Route path="/departments" element={<ListDepartmentComponent />} />
-                  <Route path="/add-department" element={<DepartmentComponent />} />
-                  <Route path="/edit-department/:id" element={<DepartmentComponent />} />
-                  <Route path="/login" element={<LoginComponent />} />
-                </Routes>
-
                 <FooterComponent />
               </>
-            </RequireAuth>
-          }
-        />
-      </Routes>
+            }
+          >
+            <Route path="/" element={<ListEmployeeComponent />} />
+            <Route path="/employees" element={<ListEmployeeComponent />} />
+            <Route path="/add-employee" element={<EmployeeComponent />} />
+            <Route path="/edit-employee/:id" element={<EmployeeComponent />} />
+            <Route path="/departments" element={<ListDepartmentComponent />} />
+            <Route path="/add-department" element={<DepartmentComponent />} />
+            <Route path="/edit-department/:id" element={<DepartmentComponent />} />
+          </Route>
+        </Route>
 
+      </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
