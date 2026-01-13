7e4cc204d29f929c54818b25798bd0fff0855960
Sohail Khan Pattan <69030926+sohail9290@users.noreply.github.com>
2026-01-12 22:35:16 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,93 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import HeaderComponent from "./components/HeaderComponent";
-import FooterComponent from "./components/FooterComponent";
-import LoginComponent from "./components/LoginComponent";
-import ListEmployeeComponent from "./components/ListEmployeeComponent";
-import EmployeeComponent from "./components/EmployeeComponent";
-import ListDepartmentComponent from "./components/ListDepartmentComponent";
-import DepartmentComponent from "./components/DepartmentComponent";
-import RequireAuth from "./auth/RequireAuth";
+import './App.css'
+import EmployeeComponent from './components/EmployeeComponent'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import ListDepartmentComponent from './components/ListDepartmentComponent'
+import DepartmentComponent from './components/DepartmentComponent'
+import LoginComponent from './components/LoginComponent'
+import RequireAuth from './auth/RequireAuth'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
 
+      <HeaderComponent />
+
+      <Routes>
         {/* PUBLIC */}
         <Route path="/login" element={<LoginComponent />} />
 
         {/* PROTECTED */}
-        <Route element={<RequireAuth />}>
-          <Route
-            element={
-              <>
-                <HeaderComponent />
-                <OutletWrapper />
-                <FooterComponent />
-              </>
-            }
-          >
-            <Route path="/" element={<ListEmployeeComponent />} />
-            <Route path="/employees" element={<ListEmployeeComponent />} />
-            <Route path="/add-employee" element={<EmployeeComponent />} />
-            <Route path="/edit-employee/:id" element={<EmployeeComponent />} />
-            <Route path="/departments" element={<ListDepartmentComponent />} />
-            <Route path="/add-department" element={<DepartmentComponent />} />
-            <Route path="/edit-department/:id" element={<DepartmentComponent />} />
-          </Route>
-        </Route>
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <ListEmployeeComponent />
+            </RequireAuth>
+          }
+        />
 
+        <Route
+          path="/employees"
+          element={
+            <RequireAuth>
+              <ListEmployeeComponent />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/add-employee"
+          element={
+            <RequireAuth>
+              <EmployeeComponent />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/edit-employee/:id"
+          element={
+            <RequireAuth>
+              <EmployeeComponent />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/departments"
+          element={
+            <RequireAuth>
+              <ListDepartmentComponent />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/add-department"
+          element={
+            <RequireAuth>
+              <DepartmentComponent />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/edit-department/:id"
+          element={
+            <RequireAuth>
+              <DepartmentComponent />
+            </RequireAuth>
+          }
+        />
       </Routes>
+
+      <FooterComponent />
+
     </BrowserRouter>
-  );
+  )
 }
 
-function OutletWrapper() {
-  return <Outlet />;
-}
-
-export default App;
+export default App
